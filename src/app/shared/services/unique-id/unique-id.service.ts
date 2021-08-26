d0ade11a3cb8f887a5bdd47f15d684f60107118b
Michel Figueira <michelfigueiratemp@gmail.com>
2021-08-26 11:44:04 -0300

    Firsts Tests with Jasmine and Karma

--- src/app/shared/services/unique-id/unique-id.service.ts
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -5,13 +5,14 @@ import { v4 as uuidv4 } from 'uuid';
 export class UniqueIdService {
 
   private numberOfGeneratedIds = 0;
+  private validId = /^[A-Za-z]+[\q\-\:\.]*$/;
 
   private generateUniqueId(): string {
     return uuidv4();
   }
 
   public generateUniqueIdWithPrefix(prefix: string): string {
-    if (!prefix) {
+    if (!prefix || !this.validId.test(prefix)) {
       throw Error('Prefix can not be empty');
     }
     const uniqueId = this.generateUniqueId();
